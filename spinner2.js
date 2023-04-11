1a2234a1173562b191f3856d413f2bb4ec69dac6
Rida Batool <ridabatool.be13@gmail.com>
2023-04-11 19:34:49 -0400

    spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,18 @@
+//process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+const spinner2 = function() {
+  const myArray = ['|   ', '/   ', '-   ', '\\   ', '|   ', '/   ', '-   '];
+  let delay = 0;
+  for (let item of myArray) {
+    setTimeout(() => {
+      process.stdout.write('\r' + item);
+    }, delay);
+    delay += 200;
+  }
+
+  setTimeout(() => {
+    console.log('');
+  }, delay);
+};
+
+spinner2();
